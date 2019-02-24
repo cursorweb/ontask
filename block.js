05fcf2ba03c414edae9c78d11b2309e9c0d61f2e
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2019-02-24 10:14:17 -0600

    Update block.js

--- block.js
+++ block.js
@@ -1,6 +1,11 @@
 chrome.storage.sync.get(['site'], function(items) {
   var g;
   console.log('Settings retrieved', items);
-  g = items;
-  console.log(g);
+  g = items.site;
+  if(g.length > 0){g.forEach(function(i){
+      if(window.location.href.indexOf(i) !== -1){}
+      else{
+          alert("bad site!");
+      }
+  });}
 });
