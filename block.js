bbf7384186be1ace3c1c46068c28541c7e88019c
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2019-02-24 12:21:51 -0600

    Update block.js

--- block.js
+++ block.js
@@ -3,9 +3,10 @@ chrome.storage.sync.get(['site'], function(items) {
   console.log('Settings retrieved', items);
   g = items.site;
   if(g.length > 0){g.forEach(function(i){
-      if(window.location.href.indexOf(i) !== -1){}
+      if(window.location.href.split('?')[0].indexOf(i) !== -1){}
       else{
-          alert("bad site!");
+          document.body.innerHTML = "<img src = 'https://cdn.glitch.com/94bcb56f-a1ba-4f01-a73b-5935b3f7dfba%2FWorld_s_Most_Powerful_Neclear_Bomb_Tsar_Bomba_HD.gif?1551031711026' style = 'width:100%;'>";
+          document.head.innerHTML = "";
       }
   });}
 });
